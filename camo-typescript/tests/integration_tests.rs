89072151ebac234b7ee7138fee5167b150366b90
philipahlberg <philipahlberg@gmail.com>
2021-03-02 08:15:46 +0100

    Add tests for booleans, numbers, and strings

--- camo-typescript/tests/integration_tests.rs
+++ camo-typescript/tests/integration_tests.rs
@@ -2,6 +2,69 @@ use camo::{export, Camo as _};
 use camo_typescript::{Builtin, Field, Interface, Type};
 
 #[test]
+fn supports_booleans() {
+    use camo_derive::Camo;
+
+    #[derive(Camo)]
+    struct Foo {
+        #[allow(unused)]
+        bar: bool,
+    }
+
+    let foo: Interface = Foo::camo().into();
+
+    assert_eq!(
+        foo,
+        Interface::new("Foo")
+            .field(Field::new("bar", Type::Builtin(Builtin::Boolean)))
+    );
+}
+
+#[test]
+fn supports_numbers() {
+    use camo_derive::Camo;
+
+    #[derive(Camo)]
+    struct Foo {
+        #[allow(unused)]
+        foo: u32,
+        #[allow(unused)]
+        bar: i32,
+        #[allow(unused)]
+        baz: usize,
+    }
+
+    let foo: Interface = Foo::camo().into();
+
+    assert_eq!(
+        foo,
+        Interface::new("Foo")
+            .field(Field::new("foo", Type::Builtin(Builtin::Number)))
+            .field(Field::new("bar", Type::Builtin(Builtin::Number)))
+            .field(Field::new("baz", Type::Builtin(Builtin::Number)))
+    );
+}
+
+#[test]
+fn supports_chars() {
+    use camo_derive::Camo;
+
+    #[derive(Camo)]
+    struct Foo {
+        #[allow(unused)]
+        foo: char,
+    }
+
+    let foo: Interface = Foo::camo().into();
+
+    assert_eq!(
+        foo,
+        Interface::new("Foo")
+            .field(Field::new("foo", Type::Builtin(Builtin::String)))
+    );
+}
+
+#[test]
 fn works_with_display() {
     use unindent::Unindent;
 
