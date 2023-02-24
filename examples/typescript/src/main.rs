c17ace6c335078d48a4523478ee1b2da8653e399
Philip Ahlberg <philipahlberg@gmail.com>
2023-02-24 23:19:04 +0100

    Simplify typescript example

--- examples/typescript/src/main.rs
+++ examples/typescript/src/main.rs
@@ -1,50 +1,28 @@
 #![allow(dead_code)]
 
-use camo::{core::Camo as _, typescript::Definition};
+use camo::{core::Camo as _, derive::Camo, typescript::Definition};
 use clap::Parser;
+use serde::Serialize;
 use std::fs::File;
 use std::io::Write as _;
 
-mod types {
-    use camo::derive::Camo;
-    use serde::Serialize;
-
-    #[derive(Camo, Serialize)]
-    #[serde(rename_all = "camelCase")]
-    pub struct Foo {
-        field_one: u32,
-        field_two: bool,
-        field_three: String,
-        field_four: Vec<i32>,
-    }
-
-    #[derive(Camo, Serialize)]
-    #[serde(rename_all = "camelCase")]
-    pub enum ExternallyTagged {
-        FirstVariant(String),
-        SecondVariant(Vec<i32>),
-    }
-
-    #[derive(Camo, Serialize)]
-    #[serde(rename_all = "camelCase", tag = "type")]
-    pub enum InternallyTagged {
-        FirstVariant(Foo),
-        SecondVariant { name: String, value: i32 },
-    }
-
-    #[derive(Camo, Serialize)]
-    #[serde(rename_all = "camelCase", tag = "type", content = "value")]
-    pub enum AdjacentlyTagged {
-        FirstVariant(String),
-        SecondVariant { values: Vec<i32> },
-    }
-
-    #[derive(Camo, Debug)]
-    pub struct NewType(i32);
-
-    #[derive(Camo, Debug)]
-    pub struct Generic<T>(T);
+#[derive(Camo, Serialize)]
+#[serde(tag = "type", content = "value")]
+pub enum Session {
+    Active(User),
+    Inactive,
 }
+
+#[derive(Camo, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct User {
+    id: UserId,
+    email: String,
+    display_name: String,
+}
+
+#[derive(Camo, Serialize)]
+pub struct UserId(String);
 
 #[derive(Parser)]
 enum Command {
@@ -53,14 +31,10 @@ enum Command {
 }
 
 fn main() -> std::result::Result<(), std::io::Error> {
-    struct T;
     let exports: &[Definition] = &[
-        types::Foo::camo().into(),
-        types::ExternallyTagged::camo().into(),
-        types::InternallyTagged::camo().into(),
-        types::AdjacentlyTagged::camo().into(),
-        types::NewType::camo().into(),
-        types::Generic::<T>::camo().into(),
+        Session::camo().into(),
+        User::camo().into(),
+        UserId::camo().into(),
     ];
 
     match Command::parse() {
